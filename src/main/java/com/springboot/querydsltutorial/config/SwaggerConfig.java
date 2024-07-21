984e05532df2941a46f6eeaed6c634df3cbde8d6
kkongdo <kkongdoes@naver.com>
2024-07-22 00:56:46 +0900

    refactor: definition을 사용하기 위한 annotation 설정

--- src/main/java/com/springboot/querydsltutorial/config/SwaggerConfig.java
+++ src/main/java/com/springboot/querydsltutorial/config/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package com.springboot.querydsltutorial.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @Info(title = "QueryDSL API 명세서",
+                description = "QueryDSL를 학습하기 위한 API 명세서",
+                version = "v1"))
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -15,8 +20,8 @@ public class SwaggerConfig {
                 .info(apiInfo());
     }
 
-    private Info apiInfo() {
-        return new Info()
+    private io.swagger.v3.oas.models.info.Info apiInfo() {
+        return new io.swagger.v3.oas.models.info.Info()
                 .title("Spring Boot REST API Specifications")
                 .description("Specification")
                 .version("1.0.0");
