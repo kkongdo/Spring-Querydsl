147f3da650cdb2440c305eded593f29886367ad7
kkongdo <kkongdoes@naver.com>
2024-08-20 17:47:56 +0900

    feat: CI 추가

--- src/test/java/com/springboot/querydsltutorial/QueryDslTutorialApplicationTests.java
+++ src/test/java/com/springboot/querydsltutorial/QueryDslTutorialApplicationTests.java
@@ -1,13 +1,1 @@
-package com.springboot.querydsltutorial;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class QueryDslTutorialApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+g
